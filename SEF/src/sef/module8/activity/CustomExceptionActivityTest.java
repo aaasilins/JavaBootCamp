121ecef377b2fcb331244f75a358505664d1a31e
Andrejs Silins <aaasilins@gmail.com>
2020-10-23 09:05:34 +0300

    Thursday commit

--- SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
+++ SEF/src/sef/module8/activity/CustomExceptionActivityTest.java
@@ -7,9 +7,14 @@ public class CustomExceptionActivityTest {
 
 	//1 - Type main method and call validateUser() from it.
 	//Call this method with different names to test it 
+	
+	public static void main(String[] args) throws CustomExceptionActivity {
+		CustomExceptionActivityTest cust = new CustomExceptionActivityTest();
+		cust.validateUser("Shanti");
+	}
 
 
-void validateUser(String name)
+void validateUser(String name) throws CustomExceptionActivity
 {
 	//array of names
 String []validUsers={"John","Mike","Shanti","Stacie"};
@@ -19,11 +24,18 @@ for(int i=0;i<4;i++)
 {
 	
 //2 - Write code to check if parameter name contains a value which is found in validUsers array and change flag's value accordingly 
-
+if (validUsers[i] == name) {
+	flag = 1;
+}
+	
 }
 //3 - check if flag is zero, throw CustomExceptionActivity Exception.
 //You may also have to declare this exception in the method call using throws.
-
+if (flag == 0) {
+	throw new CustomExceptionActivity();
+} else {
+	System.out.println("Welcome to Payroll program");
+}
 
 //4 - else if flag is one, print a message "Welcome to Payroll program". 
 
--- SEF/src/sef/module8/activity/SQLExceptionActivity.java
+++ SEF/src/sef/module8/activity/SQLExceptionActivity.java
@@ -17,13 +17,17 @@ public class SQLExceptionActivity {
 			//The following code would not compile unless it's put inside a try catch
 			//1 - put it in a try block and handle ClassNotFoundException
 			
-			
+			try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection cn = DriverManager.getConnection(url, user, pass);
 			System.out.println("Connection successfully established! \n");
 			
 			cn.close();
-			
+			} catch (SQLException e) {
+				System.out.println("Can't connect");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class not found");
+			}
 			//2 - You also need to catch SQLException for it to compile
 			
 	
